d027346a11772339402498430c08d4025d52f919
Mendes16 <33485565+Mendes16@users.noreply.github.com>
2017-11-29 11:19:14 -0200

    Add files via upload

---
+++ modeloLojaIFC-master/app/models/Produto.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JEFFERSON
+ * Date: 09/11/2017
+ * Time: 10:40
+ */
+
+require_once "Conexao.php";
+
+class Produto {
+
+    public $codigo;
+    public $nome;
+    public $preco;
+    public $categoria;
+    public $quantidadeEstoque;
+
+    public function __construct($nome, $preco, $categoria, $estoque){ //estoque
+        $this->nome = $nome;
+        $this->preco = $preco;
+        $this->categoria = $categoria;
+        $this->quantidadeEstoque = $estoque;
+    }
+
+    public function estaDisponivel(){
+        if($this->quantidadeEstoque > 0)
+        {
+            return "disponivel";
+        } else {
+            return "indisponivel";
+        }
+    }
+
+}
